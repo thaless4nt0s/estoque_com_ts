a2380549130ad93b863088a47b4ffb38d812ad84
= <=>
2022-08-02 10:07:22 -0300

    Buscar todos os fornecedores

--- src/controllers/providerController.ts
+++ src/controllers/providerController.ts
@@ -2,11 +2,21 @@ import { Request, Response } from "express";
 import { provider } from "../models/provider";
 import Logger from '../../config/logger';
 
-export default async function createProvider(req: Request, res: Response){
+export async function createProvider(req: Request, res: Response){
     try{
         const providerCreated = await provider.create(req.body);
-        res.status(200).json(providerCreated);
+        return res.status(200).json(providerCreated);
     }catch(e: any){
         Logger.error(`Erro ao criar fornecedor: ${e.message}`);
     }
 };
+
+export async function findAllProviders(req: Request, res: Response){
+    try{
+        const providers = await provider.find();
+        return res.status(200).json(providers);
+    }catch(e: any){
+        Logger.error(`Erro ao buscar os fornecedores: ${e.message}`);
+        return res.status(500).json({error: `Busque novamente mais tarde`})
+    }
+}
--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
 import express, { Router, Request, Response } from 'express';
-import createProvider from './controllers/providerController';
+import {createProvider, findAllProviders} from './controllers/providerController';
 
 const router = Router();
 
 router.get("/", (req: Request, res: Response) =>{
     res.status(200).send("Funcionando !!");
-}).post('/create/provider', createProvider);
+}).post('/create/provider', createProvider)
+.get("/provider", findAllProviders)
+;
 
 export default router;
