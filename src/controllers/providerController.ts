20dd153bf98674eab434738ee87037780d45e539
= <=>
2022-08-02 09:55:10 -0300

    função para criar fornecedor

--- src/controllers/providerController.ts
+++ src/controllers/providerController.ts
@@ -1,3 +1,12 @@
 import { Request, Response } from "express";
 import { provider } from "../models/provider";
+import Logger from '../../config/logger';
 
+export default async function createProvider(req: Request, res: Response){
+    try{
+        const providerCreated = await provider.create(req.body);
+        res.status(200).json(providerCreated);
+    }catch(e: any){
+        Logger.error(`Erro ao criar fornecedor: ${e.message}`);
+    }
+};
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import express, { Router, Request, Response } from 'express';
+import createProvider from './controllers/providerController';
 
 const router = Router();
 
 router.get("/", (req: Request, res: Response) =>{
     res.status(200).send("Funcionando !!");
-})
+}).post('/create/provider', createProvider);
 
 export default router;
