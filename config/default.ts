9c6bd9051e851d82acfd19141934f6638ef56d80
= <=>
2022-08-02 09:38:55 -0300

    Adicionando as conexões com o banco de dados

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,7 @@
 export default{
-    port: 3001
+    port: 3001,
+    user: "thales",
+    password: "a0rmNd5CR6r4Jjvf",
+    url: "mongodb+srv://thales:a0rmNd5CR6r4Jjvf@cluster0.inxdx.mongodb.net/?retryWrites=true&w=majority",
+    env: "development"
 };
