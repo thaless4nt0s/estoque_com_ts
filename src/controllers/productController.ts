73fed92f8d7ae81f4210e82233b3f55fd5b30673
= <=>
2022-08-04 20:01:17 -0300

    Correção na funcao de criar produto

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -3,10 +3,17 @@ import { Request, Response } from "express";
 // Schema
 import {product} from '../models/product';
 
+//Logs de erro
+import Logger from '../../config/logger';
+
 export async function createProduct(req: Request, res: Response){
-    const data = req.body;
-    const createdProduct = await product.create(data);
-    console.log(createdProduct);
-    return res.status(200).json(createdProduct);
+    try{
+        const data = req.body;
+        const createdProduct = await product.create(data);
+        return res.status(200).json(createdProduct);
+    }catch(e: any){
+        Logger.error(`Não foi possível criar o produto: ${e.message}`);
+        return res.status(404).json({msg: `Tente novamente mais tarde`});
+    }
 }
 
